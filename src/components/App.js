ba99ff3cfc1cacc8ca3855c0bf4257859dc5a8ce
sainigorv <sainigorv341@gmail.com>
2022-12-29 01:19:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  // const[input, setInput] = React.useState("")
+  // const[inputNum, setInputNum] = React.useState("")
+  const handleInput = (event) => {
+    // use console.log
+    //  setInput(event.target.value)
+    console.log(`Input in #${event.target.id} is ${event.target.value}`);
+  };
 
-  const handleInput = (event) =>{
-   // use console.log
-  }
+  // const handleInputNum = (event) =>{
+  //   // use console.log
+  //   setInputNum(event.target.value)
+  //   console.log(event.target.value)
+  //  }
 
   // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <label htmlFor="text-input">Text Input:- </label>
+      <input id="text-input" onChange={handleInput} type={"text"} />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <label htmlFor="num-input">Number input</label>
+      <input id="num-input" onChange={handleInput} type={"number"} />
+      <br />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
